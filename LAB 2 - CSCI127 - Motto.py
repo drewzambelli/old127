74183bfc1fae3cf0602c2e16d2510707012096ef
drewzambelli <86168375+drewzambelli@users.noreply.github.com>
2022-10-18 01:52:31 -0400

    Add files via upload

---
+++ LAB 2 - CSCI127 - Motto.py
@@ -0,0 +1,21 @@
+#Name: Drew Zambelli
+#Email: drew.zambelli04@myhunter.cuny.edu
+#Date: September 13, 2021
+#This program prints user input in upper and lower case and counts occurence of letter entered
+
+
+
+message = input("enter a message:")
+letter = input("enter a letter:")
+
+print(message)
+print(message.upper())
+print(message.lower())
+
+counter = message.count(letter)
+
+print(counter)
+
+
+
+
