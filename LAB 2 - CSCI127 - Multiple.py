eef6714e336ab4d0b66e38d9ca6ea640d5a13c00
drewzambelli <86168375+drewzambelli@users.noreply.github.com>
2022-10-18 01:52:39 -0400

    Add files via upload

---
+++ LAB 2 - CSCI127 - Multiple.py
@@ -0,0 +1,9 @@
+#Name: Drew Zambelli
+#Email: drew.zambelli04@myhunter.cuny.edu
+#Date: September 14, 2021
+#This program does Multiples
+
+for i in range (2,21,2):
+    initialValue = i
+    finalValue = initialValue + 10
+    print("current value: {} | value+10: {}".format(initialValue, finalValue))
