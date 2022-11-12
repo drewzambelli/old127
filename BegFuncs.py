8fe6396e983832daeb1d4144cc59a95f045d6678
drewzambelli <86168375+drewzambelli@users.noreply.github.com>
2022-11-12 13:55:33 -0500

    Add files via upload

---
+++ BegFuncs.py
@@ -0,0 +1,13 @@
+
+#Name: Drew Zambelli
+#Email: drew.zambelli04@myhunter.cuny.edu
+#Date: October 13, 2021
+#This program prints Hello Functions!
+
+def main():
+    num = int(input("Enter a number:"))
+    for i in range(num):
+        print("Hello, Functions!")
+
+if __name__== "__main__":
+    main()
